f1d011bfa4b7da002cb840371a1b79df0a0d8de8
Kouji_Kimura <kimura@ps-econet.co.jp>
2017-10-10 17:57:43 +0900

    Initial commit

---
+++ android/src/p/techacademy/hirokazu/tomoda/jumpactiongame/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package p.techacademy.hirokazu.tomoda.jumpactiongame;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import p.techacademy.hirokazu.tomoda.jumpactiongame.JumpActionGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new JumpActionGame(), config);
+	}
+}
